6cf52ee7913f26939f34645ce84dd66c3f1265fb
AlexeyGerashchenko <aagerashchenko@edu.hse.ru>
2025-02-11 20:51:28 +0300

    Архитектура

--- go.mod
+++ go.mod
@@ -1,1 +1,8 @@
 module KPO-miniHW-1
+
+go 1.23.4
+
+require (
+	github.com/eiannone/keyboard v0.0.0-20220611211555-0d226195f203 // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+)
--- main.go
+++ main.go
@@ -2,13 +2,63 @@ package main
 
 import (
 	"fmt"
+	"github.com/eiannone/keyboard"
 )
 
 func main() {
-	s := "gopher"
-	fmt.Println("Hello and welcome, %s!", s)
 
-	for i := 1; i <= 5; i++ {
-		fmt.Println("i =", 100/i)
+	if err := keyboard.Open(); err != nil {
+		panic(err)
+	}
+	defer func() {
+		_ = keyboard.Close()
+	}()
+
+	fmt.Println("Нажмите стрелку влево, стрелку вправо или Esc для выхода...")
+	fmt.Println("Добро пожаловать!")
+	fmt.Println("\nМеню действий")
+	fmt.Println("\nЧтобы выйти, нажми кнопку 5")
+
+	for {
+		char, key, err := keyboard.GetKey()
+		if err != nil {
+			panic(err)
+		}
+
+		if key == keyboard.KeyArrowLeft {
+			fmt.Println("Стрелка влево нажата")
+		} else if key == keyboard.KeyArrowRight {
+			fmt.Println("Стрелка вправо нажата")
+		} else if key == keyboard.KeyEsc {
+			fmt.Println("Выход")
+			break
+		}
+
+		if char == 'q' || char == 'Q' {
+			fmt.Println("Выход")
+			break
+		}
 	}
 }
+
+//package main
+//
+//import (
+//	"fmt"
+//)
+//
+//func main() {
+//	s := "gopher"
+//	fmt.Println("Hello and welcome, %s!", s)
+//
+//	for {
+//		fmt.Println("Добро пожаловать!")
+//		fmt.Println("\nМеню действий")
+//		fmt.Println("\nЧтобы выйти, нажми кнопку 5")
+//
+//	}
+//
+//	for i := 1; i <= 5; i++ {
+//		fmt.Println("i =", 100/i)
+//	}
+//}
